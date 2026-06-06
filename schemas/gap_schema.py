578baff8f5b4c95080131a3a666ba65935bcd858
lalchhabi <tmgchhabi555@gmail.com>
2026-06-06 17:04:58 +0545

    fix: solved the data typing issues

--- schemas/gap_schema.py
+++ schemas/gap_schema.py
@@ -1,11 +1,10 @@
 # Import libraries
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import List
 
 
 # Define Gap Information Schema
 class GapSchema(BaseModel):
-
     match_score: int = Field(description="Overall compatibility score between resume and job requirements")
     strong_skills: List[str] = Field(default_factory=list,
                                      description="Skills from the resume that strongly match the job requirements")
