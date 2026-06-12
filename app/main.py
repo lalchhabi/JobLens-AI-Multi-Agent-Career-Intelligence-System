538c986a92f4ea5ea5e042336c7736319ea7448c
lalchhabi <tmgchhabi555@gmail.com>
2026-06-12 23:34:36 +0545

    fix: update FastAPI routes and Jinja2 template integration

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # Import libraries
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 # Import project files
 from api.routes import router
@@ -8,7 +9,10 @@ app = FastAPI(
     title='JobLens AI',
     description="AI career Intelligence System",
     version="1.0.0"
-
 )
+
+# Serve static files
+app.mount("/static", StaticFiles(directory="ui/static"), name =
+              "static")
 
 app.include_router(router)
